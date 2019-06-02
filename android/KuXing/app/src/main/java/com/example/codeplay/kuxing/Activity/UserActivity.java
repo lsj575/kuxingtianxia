bcba8ac042d6c3c9f17919c9784f78ce9e72ac09
UncleJ <sue12160@163.com>
2019-06-02 11:21:47 +0800

    用户主页界面

---
+++ android/KuXing/app/src/main/java/com/example/codeplay/kuxing/Activity/UserActivity.java
@@ -0,0 +1,31 @@
+package com.example.codeplay.kuxing.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class UserActivity extends AppCompatActivity {
+    public UserActivity() {
+        super();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
