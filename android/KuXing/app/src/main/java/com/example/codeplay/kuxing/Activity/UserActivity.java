da0cc6ee6699202da47333ec73b350e4235ce5ab
UncleJ <sue12160@163.com>
2019-06-03 08:58:39 +0800

    用户主页界面+发信息图标

--- android/KuXing/app/src/main/java/com/example/codeplay/kuxing/Activity/UserActivity.java
+++ android/KuXing/app/src/main/java/com/example/codeplay/kuxing/Activity/UserActivity.java
@@ -3,6 +3,14 @@ package com.example.codeplay.kuxing.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.example.codeplay.kuxing.R;
 
 public class UserActivity extends AppCompatActivity {
     public UserActivity() {
@@ -12,6 +20,8 @@ public class UserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
     }
 
     @Override
--- android/KuXing/app/src/main/java/com/example/codeplay/kuxing/Fragment/FragmentMy.java
+++ android/KuXing/app/src/main/java/com/example/codeplay/kuxing/Fragment/FragmentMy.java
@@ -12,7 +12,7 @@ import com.example.codeplay.kuxing.R;
 public class FragmentMy extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.frag_my,container,false);
+        View view=inflater.inflate(R.layout.activity_user,container,false);
         return view;
     }
 
